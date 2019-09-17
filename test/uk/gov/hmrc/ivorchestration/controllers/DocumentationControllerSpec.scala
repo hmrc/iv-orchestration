dd5e8d9293cc104ba7eb319030b06ebfbca186b9
Pasquale Gatto <pasquale.gatto@hmrc.digital.gov.uk>
2019-09-17 15:14:50 +0100

    TIV-216: Remove unused endpoint

--- test/uk/gov/hmrc/ivorchestration/controllers/DocumentationControllerSpec.scala
+++ test/uk/gov/hmrc/ivorchestration/controllers/DocumentationControllerSpec.scala
@@ -27,28 +27,11 @@ import play.api.test.Helpers._
 class DocumentationControllerSpec extends BaseSpec with GuiceOneAppPerSuite with MockFactory {
   implicit val hc = HeaderCarrier()
 
-
-  "provide definition endpoint and documentation endpoint for each api" in new Setup {
-    def normalizeEndpointName(endpointName: String): String = endpointName.replaceAll(" ", "-")
-
-    def verifyDocumentationPresent(version: String, endpointName: String) {
-      withClue(s"Getting documentation version '$version' of endpoint '$endpointName'") {
-        val documentationResult = documentationController.documentation(version, endpointName)(request)
-        status(documentationResult) mustBe OK
-      }
-    }
-
+  "provide definition endpoint for each api" in new Setup {
     val result = documentationController.definition()(request)
+    contentAsJson(result)
+
     status(result) mustBe OK
-
-    val jsonResponse = contentAsJson(result)
-
-    val versions: Seq[String] = (jsonResponse \\ "version") map (_.as[String])
-    val endpointNames: Seq[Seq[String]] = (jsonResponse \\ "endpoints").map(_ \\ "endpointName").map(_.map(_.as[String]))
-
-    versions.zip(endpointNames).flatMap {
-      case (version, endpoint) => endpoint.map(endpointName => (version, endpointName))
-    }.foreach { case (version, endpointName) => verifyDocumentationPresent(version, endpointName) }
   }
 
   "provide raml documentation" in new Setup {
@@ -58,11 +41,8 @@ class DocumentationControllerSpec extends BaseSpec with GuiceOneAppPerSuite with
     contentAsString(result) must startWith("#%RAML 1.0")
   }
 
-
-//  private val mockAssets = mock[Assets]
-
   trait Setup {
-    implicit def mat: akka.stream.Materializer = app.injector.instanceOf[akka.stream.Materializer]
+    implicit def materializer: akka.stream.Materializer = app.injector.instanceOf[akka.stream.Materializer]
     val documentationController = app.injector.instanceOf[DocumentationController]
     val request = FakeRequest()
   }
