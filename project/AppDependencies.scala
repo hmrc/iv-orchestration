d29e4048829783de3207bf34881fca7da4e76a2e
Stephen Kam <20183211+Stephen-Kam@users.noreply.github.com>
2020-07-21 16:14:26 +0100

    SIS-997 upgrade simple mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"                %% "simple-reactivemongo"     % "7.22.0-play-26",
+    "uk.gov.hmrc"                %% "simple-reactivemongo"     % "7.30.0-play-26",
     "uk.gov.hmrc"                %% "bootstrap-play-26"        % "0.42.0",
     "uk.gov.hmrc"                %% "play-hmrc-api"            % "3.6.0-play-26",
     "com.typesafe.play"          %% "play-json-joda"           % "2.6.7",
