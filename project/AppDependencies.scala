081c353c0209ed7a4f24d3fba7d25ce0091ab90a
DarylMConway <daryl.conway@digital.hmrc.gov.uk>
2021-06-08 13:40:49 +0100

    VER-887 start of play 2.8 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,9 +4,9 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"                %% "simple-reactivemongo"       % "7.31.0-play-27",
-    "uk.gov.hmrc"                %% "bootstrap-backend-play-27"  % "3.2.0",
-    "uk.gov.hmrc"                %% "play-hmrc-api"              % "5.3.0-play-27",
+    "uk.gov.hmrc"                %% "simple-reactivemongo"       % "8.0.0-play-28",
+    "uk.gov.hmrc"                %% "bootstrap-backend-play-28"  % "5.2.0",
+    "uk.gov.hmrc"                %% "play-hmrc-api"              % "6.2.0-play-28",
     "com.typesafe.play"          %% "play-json-joda"             % "2.6.7",
     "com.github.pureconfig"      %% "pureconfig"                 % "0.12.3",
     "org.typelevel"              %% "cats-core"                  % "2.1.1",
@@ -15,9 +15,9 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "org.scalamock"           %% "scalamock"                % "4.4.0"                 % "test",
+    "org.scalamock"           %% "scalamock"                % "5.1.0"                 % "test",
     "com.typesafe.play"       %% "play-test"                % current                 % "test",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"                 % "test, it"
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                 % "test, it"
   )
 }
