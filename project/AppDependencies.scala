a0ad90337e4b9125a7c02ce3f4669685c165a8b2
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2025-08-29 09:42:04 +0100

    [VER-5805]Updated bootstrap play

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,10 +1,10 @@
 import play.core.PlayVersion.current
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  val bootStrapVersion: String = "9.11.0"
-  val mongoVersion: String = "2.6.0"
+  val bootStrapVersion: String = "9.14.0"
+  val mongoVersion: String = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"         % mongoVersion,
@@ -13,11 +13,10 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"   % bootStrapVersion        % Test,
-    "org.scalamock"           %% "scalamock"                % "7.3.2"                 % Test,
-    "org.playframework"       %% "play-test"                % current                 % Test,
-    "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % Test,
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "7.0.1"                 % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.64.8"                % Test
-  )
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"   % bootStrapVersion,
+    "org.scalamock"           %% "scalamock"                % "7.4.1",
+    "org.playframework"       %% "play-test"                % current,
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "7.0.2",
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.64.8"
+  ).map(_ % "test")
 }
